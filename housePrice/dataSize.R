fa89917cc104817508fe2eb475fea42e537eadda
ma <ma@MacBook-Pro.local>
2020-07-10 18:20:38 +0900

    first commit

---
+++ housePrice/dataSize.R
@@ -0,0 +1,22 @@
+dim(train)
+str(train[,c(1:10, 81)])
+test_labels <- test$Id
+test$Id <- NULL
+train$Id <- NULL
+
+test$SalePrice <- NA
+all <- rbind(train, test)
+dim(all)
+
+ggplot(data = all[!is.na(all$SalePrice),], aes(x=SalePrice)) +
+  geom_histogram(fill='blue', binwidth = 10000) +
+  scale_x_continuous(breaks = seq(0, 8000000,by=100000), labels = comma)
+
+summary(all$SalePrice)
+numericVars <- which(sapply(all, is.numeric))
+numericVarNames <- names(numericVars)
+
+cat("there are ", length(numericVars), "numeric variables")
+
+all_numVar <- all[, numericVars]
+
