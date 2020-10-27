059330b9c5517596507640c35718f50ea8c2a718
ma <ma_yamaki@yahoo.com>
2020-10-27 17:28:59 +0900

    tukareata

--- future_sales/code/misc.py
+++ future_sales/code/misc.py
@@ -41,10 +41,49 @@ train = train[train.item_price<100000]
 train = train[train.item_cnt_day<1001]
 
 train.loc[train.shop_id == 0, 'shop_id'] = 57
-test.loc[test.shop_id == 0, 'shop_id'] =57
+test.loc[test.shop_id == 0, 'shop_id'] = 57
 
 train.loc[train.shop_id == 1, 'shop_id'] = 58
 test.loc[test.shop_id == 1, 'shop_id'] = 58
 
-train.loc[train.shop_id == 10, 'shop_id'] == 11
-test.loc[test.shop_id == 10, 'shop_id'] == 11
+train.loc[train.shop_id == 10, 'shop_id'] = 11
+test.loc[test.shop_id == 10, 'shop_id'] = 11
+
+
+shops.loc[shops.shop_name == 'Сергиев Посад ТЦ "7Я"', 'shop_id'] = 'СергиевПосад ТЦ "7Я"'
+shops['city'] = shops['shop_name'].str.split(' ').map(lambda x:  x[0])
+shops.loc[shops.city == '!Якутск', 'city'] = 'Якутск'
+shops['city_code'] = LabelEncoder().fit_transform(shops['city'])
+shops = shops[['shop_id','city_code']]
+
+cats['split'] = cats['item_category_name'].str.split('-')
+cats['type'] = cats['split'].map(lambda x: x[0].strip())
+cats['type_code'] = LabelEncoder().fit_transform(cats['type'])
+
+cats['subtype'] = cats['split'].map(lambda x: x[1].strip() if len(x) > 1 else x[0].strip())
+cats['subtype_code'] = LabelEncoder().fit_transform(cats['subtype'])
+cats = cats[['item_category_id', 'type_code', 'subtype_code']]
+
+items.drop(['item_name'], axis=1, inplace=True)
+
+#Monthly sales
+
+len(list(set(test.item_id) - set(test.item_id).intersection(set(train.item_id))))
+len(list(set(test.item_id)))
+len(test)
+
+ts = time.time()
+matrix = []
+cols = ['date_block_num', 'shop_id', 'item_id']
+for i in range(34):
+    sales = train[train.date_block_num==i]
+    matrix.append(np.array(list(product([i], sales.shop_id.unique(), sales.item_id.unique())), dtype='int16'))
+
+matrix = pd.DataFrame(np.vstack(matrix), columns=cols)
+matrix['date_block_num'] = matrix['date_block_num'].astype(np.int8)
+matrix['shop_id'] = matrix['shop_id'].astype(np.int8)
+matrix['item_id'] = matrix['item_id'].astype(np.int16)
+matrix.sort_values(cols, inplace=True)
+time.time() - ts
+
+train['revenue'] = train['item_price'] * train['item_cnt_day']
