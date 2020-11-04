8fa2ceaca8e335c51431406110d6514e82c53a55
ma <ma_yamaki@yahoo.com>
2020-11-04 14:02:15 +0900

    momom

--- future_sales/code/2nd.py
+++ future_sales/code/2nd.py
@@ -32,3 +32,39 @@ print(sales.info())
 
 manthly_sales = sales.groupby(['date_block_num','shop_id'])['date','item_price','item_cnt_day']\
                                 .agg({'date':['min','max'], 'item_price':'mean', 'item_cnt_day':'sum'})
+
+x = item.groupby(['item_category_id']).count()
+x = x.sort_values(by='item_id', ascending=False)
+x = x.iloc[0:10].reset_index()
+
+plt.figure(figsize=(8,4))
+ax = sns.barplot(x.item_category_id, x.item_id, alpha=0.8)
+plt.title('Item per Category')
+plt.ylabel('# of items', fontsize=12)
+plt.xlabel("Category", fontsize=12)
+plt.show()
+
+
+ts = sales.groupby(["date_block_num"])['item_cnt_day'].sum()
+ts.astype('float')
+plt.figure(figsize=(16,8))
+plt.title("Total Sales of the company")
+plt.xlabel("Time")
+plt.ylabel("Sales")
+plt.plot(ts)
+
+plt.figure(figsize=(16,6))
+plt.plot(ts.rolling(window=12, center=False).mean(), label='Rolling Mean')
+plt.plot(ts.rolling(window=12, center=False).std(), label="Rolling sd")
+
+res = sm.tsa.seasonal_decompose(ts.values,freq=12, model='multicative')
+fig = res.plot()
+
+
+res = sm.tsa.seasonal_decompose(ts.values, freq=12, model='additive')
+fig = res.plot()
+
+def test_stationarity(timeseries):
+    print("Results of Dicky-Fuller Test:")
+    dftest = adfuller(timeseries, autolag='AIC')
+    dfoutput = pd.Series(dftest[0:4], index=['Test Static', ])
