01aa32a53d4de077e9e7b991bbb1013b5c93625b
ma <ma_yamaki@yahoo.com>
2020-12-01 14:23:10 +0900

    dddd

--- rosman/code/first.py
+++ rosman/code/first.py
@@ -8,6 +8,7 @@ from pandas import datetime
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from statsmodels.distributions.empirical_distribution import ECDF
 
 from statsmodels.tsa.seasonal import seasonal_decompose
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
@@ -21,3 +22,25 @@ train['Year'] = train.index.year
 train['Month'] = train.index.month
 train['Day'] = train.index.day
 train['WeekOfYear'] = train.index.weekofyear
+
+train['salePerCustomer'] = train['Sales'] / train['Customers']
+train['salePerCustomer'].describe()
+
+sns.set(style='ticks')
+c='#386B7F'
+
+plt.figure(figsize=(12,6))
+
+plt.subplot(311)
+cdf = ECDF(train['Sales'])
+plt.plot(cdf.x, cdf.y, label='statsmodels', color=c)
+
+plt.subplot(312)
+cdf = ECDF(train['Customers'])
+plt.plot(cdf.x, cdf.y, label='statsmodels', color=c)
+plt.xlabel('Customers')
+
+pl.subplot(313)
+cdf = ECDF(train['salePerCustomer'])
+plt.plot(cdf.x, cdf.y, label='statsmodels', color=c)
+plt.xlabel('Sale Per Customer')
