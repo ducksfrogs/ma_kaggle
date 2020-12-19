8c8b9808565ed82b3d13e70c63e3179500930588
ma <ma_yamaki@yahoo.com>
2020-12-19 09:39:29 +0900

    d

---
+++ titanic/code/ensemb_stack.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import numpy as np
+import re
+import sklearn
+import xgboost as xgb
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import plotly.offline as py
+py.init_notebook_mode(connected=True)
+import plotly.graph_objs as go
+import plotly.tools as tls
+
+import warnings
+warnings.filterwarnings('ignore')
+
+from sklearn.ensemble import (RandomForestClassifier, AdaBoostClassifier,
+                              GradientBoostingClassifier, ExtraTreesClassifier)
+
+from sklearn.svm import SVC
+from sklearn.model_selection import KFold
+
+#Load in the train and test datasets
+
+train = pd.read_csv('../input/train.csv')
+test = pd.read_csv('../input/test.csv')
+
+PassengerId = test['PassengerId']
+
+full_data = [train, test]
+
+train['Name_length'] = train['Name'].apply(len)
+test['Name_length'] = test['Name'].apply(len)
+
+train['Has_Cabin'] = train['Cabin'].apply(lambda x: 0 if type(x) == float else 1)
+test['Has_Cabin'] = test['Cabin'].apply(lambda x: 0 if type(x) == float else 1)
+
+for dataset in full_data:
+    dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1
+
+for dataset in full_data:
+    dataset['IsAlone'] = 0
+    dataset.loc[dataset['FamilySize'] == 1, 'IsAlone'] =1
+
+for dataset in full_data:
+    dataset['Embarked'] = dataset['Embarked'].fillna('S')
+
+for dataset in full_data:
+    dataset['Fare'] = dataset['Fare'].fillna(train['Fare'].median())
+
+train['CategoricalFare'] = pd.qcut(train['Fare'], 4)
+
+for dataset in full_data:
+    age_avg = dataset['Age'].mean()
+    age_std = dataset['Age'].std()
+    
