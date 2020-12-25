38edcf8da14a563a25a70f5f3a418d9adbbbe885
ducksfrogs <ma_yamaki@yahoo.com>
2020-12-25 15:29:35 +0900

    ddd

--- titanic/code/eda.py
+++ titanic/code/eda.py
@@ -43,3 +43,46 @@ grid.add_legend()
 grid = sns.FacetGrid(train_df, row='Embarked', size=2.2, aspect=1.6)
 grid.map(sns.pointplot, 'Pclass', 'Survived', 'Sex', palette='deep')
 grid.add_legend()
+
+grid = sns.FacetGrid(train_df, row='Embarked', col='Survived', size=2.2, aspect=1.6)
+grid.map(sns.barplot, 'Sex', 'Fare', alpha=0.5, ci=None)
+grid.add_legend()
+
+
+print("Befor", train_df.shape, test_df.shape, combine[0].shape, combine[1].shape)
+
+train_df = train_df.drop(['Ticket', 'Cabin'],axis=1)
+test_df= test_df.drop(['Ticket', 'Cabin'], axis=1)
+
+print("After", train_df.shape, test_df.shape, combine[0].shape, combine[1].shape)
+
+for dataset in combine:
+    dataset['Title'] = dataset.Name.str.extract(' ([A-Za-z]+)\.', expand=False)
+
+pd.crosstab(train_df['Title'], train_df['Sex'])
+
+
+for dataset in combine:
+    dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess', 'Capt', 'Col'\
+                                        'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+    dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Ms','Miss')
+    dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
+
+train_df[['Title', 'Survived']].groupby(['Title'], as_index=False).mean()
+
+title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
+
+for dataset in combine:
+    dataset['Title'] = dataset['Title'].map(title_mapping)
+    dataset['Title'] = dataset['Title'].fillna(0)
+
+
+
+train_df = train_df.drop(['Name', "PassengerId"],axis=1)
+test_df = test_df.drop(['Name', "PassengerId"],axis=1)
+
+
+
+for dataset in combine:
+    dataset['Sex'] = dataset['Sex'].map({"female": 1, "male":0}).astype(int)
