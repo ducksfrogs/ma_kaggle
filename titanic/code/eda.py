0aba0c5ba45994ccb0690470e3dceb9033814958
ducksfrogs <ma_yamaki@yahoo.com>
2020-12-25 14:21:38 +0900

    d

--- titanic/code/eda.py
+++ titanic/code/eda.py
@@ -21,3 +21,25 @@ combine = [train_df, test_df]
 train_df.info()
 print('-'*40)
 test_df.info()
+
+train_df.describe()
+
+train_df.describe(include=['O'])
+
+train_df[['Pclass','Survived']].groupby(['Pclass'],as_index=False).mean(),sort_values(by='Survived', ascending=False)
+
+train_df[['SibSp','Survived']].groupby(['SibSp'], as_index=False).mean(),sort_values(by='Survived', ascending=False)
+
+train_df[['Parch','Survived']].groupby(['Parch'], as_index=False ).mean(),sort_values(by='Survived', ascending=False)
+
+g = sns.FacetGrid(train_df, col='Survived')
+g.map(plt.hist, 'Age', bins=20)
+
+grid = sns.FacetGrid(train_df, col="Survived", row='Pclass', size=2.2, aspect=1.6)
+grid.map(plt.hist, 'Age', alpha=0.5, beta=20)
+grid.add_legend()
+
+
+grid = sns.FacetGrid(train_df, row='Embarked', size=2.2, aspect=1.6)
+grid.map(sns.pointplot, 'Pclass', 'Survived', 'Sex', palette='deep')
+grid.add_legend()
