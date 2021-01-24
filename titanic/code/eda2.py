3cee22880317798e5c90ed60103da95ffa3a82c3
ma <ma_yamaki@yahoo.com>
2021-01-24 21:33:35 +0900

    dd

--- titanic/code/eda2.py
+++ titanic/code/eda2.py
@@ -103,4 +103,33 @@ guess_ages = np.zeros((2,3))
 for dataset in combined_df:
     for i in range(0,2):
         for j in range(0,3):
-            guess_df = dataset([(dataset['Sex']==i) & (dataset[])])
+            guess_df = dataset[(dataset['Sex']==i) & (dataset['Pclass'] == j+1)]['Age'].dropna()
+
+            age_guess = guess_df.median()
+
+            guess_ages[i,j] = int(age_guess/0.5 + 0.5) *0.5
+
+    for i in range(0,2):
+        for j in range(0,3):
+            dataset.loc[(dataset.Age.isnull()) & (dataset.Sex==i) & (dataset.Pclass==j+1),
+                        'Age'] = guess_ages[i, j]
+    dataset['Age'] = dataset['Age'].astype(int)
+
+train_df['AgeBand'] = pd.cut(train_df['Age'], 5)
+train_df[['AgeBand', 'Survived']].groupby(['AgeBand'], as_index=Falsel).mean().sort_values(by='AgeBand', ascending=True)
+
+for dataset in combined_df:
+    dataset.loc[ dataset['Age'] <= 16, 'Age'] = 0
+    dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <=32), 'Age'] = 1
+    dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] =2
+    dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
+    dataset.loc[ dataset['Age'] > 64, 'Age']
+
+
+train_df = train_df.drop(['AgeBand'], axis=1)
+combined_df = [train_df, test_df]
+
+for dataset in combined_df:
+    dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1
+
+train_  
