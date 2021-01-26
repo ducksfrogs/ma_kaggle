58a616e5e70f9659512ac7b0603bc482b13bc24a
ma <ma_yamaki@yahoo.com>
2021-01-26 16:09:54 +0900

    dadada

--- titanic/code/first_stuking.py
+++ titanic/code/first_stuking.py
@@ -50,4 +50,71 @@ train['CategoricalFare'] = pd.cut(train['Fare'], 4)
 for dataset in full_data:
     age_avg = dataset['Age'].mean()
     age_std = dataset['Age'].std()
-    
+    age_null_count = dataset['Age'].isnull().sum()
+    age_null_random_list = np.random.randint(age_avg - age_std, age_avg + age_std, size=age_null_count)
+    dataset['Age'][np.isnan(dataset['Age'])] = age_null_random_list
+    dataset['Age'] = dataset['Age'].astype(int)
+
+train['CategoricalAge'] = pd.cut(train['Age'], 5)
+
+def get_title(name):
+    title_search = re.search(' ([A-Za-z]+)\.', name)
+    if title_search:
+        return title_search.group(1)
+    return ""
+
+for dataset in full_data:
+    dataset["Title"] = dataset['Name'].apply(get_title)
+
+for dataset in full_data:
+
+
+
+
+colormap = plt.cm.RdBu
+plt.figure(figsize=(14,12))
+plt.title("Peason Correlation of Features", y=1.05, size=15)
+sns.heatmap(train.astype(float).corr(), linewidths=0.1, vmax=1.0,
+        square=True, cmap=colormap, linecolor='white', annot= True)
+
+
+g = sns.pairplot(train[['Survived', 'Pclass', 'Sex', 'Age', 'Parch','Fare', 'Embarked', 'FamilySize', 'Title']],
+            hue='Survived', palette='seismic', size=1.2, diag_kind='kde',
+            diag_kws=dict(shade=True), plot_kws=dict(s=10))
+
+g.set(xticklabels=[])
+
+
+ntrain = train.shape[0]
+ntest = test.shape[0]
+SEED = 0
+NFOLDS = 5
+
+kf = KFold()
+
+
+class SklearnHelper(object):
+    def __init__(self, clf, seed=0, params=None):
+        params['random_state'] = seed
+        self.clf = clf(**params)
+
+    def train(self, X_train, y_train):
+        
+
+def get_oof(clf, X_train, y_train, X_test):
+    oof_train = np.zeros((ntrain,))
+    oof_test = np.zeros((ntest, ))
+    oof_test_skf = np.empty((NFOLDS, ntest))
+
+    for i, (train_index, test_index) in enumerate(kf):
+        x_tr = X_train[train_index]
+        y_tr = y_train[train_index]
+        x_te = X_train[test_index]
+
+        clf.train(x_tr, y_tr)
+
+        oof_train[test_index] = clf.predict(x_te)
+        oof_test_skf[i, :] = clf.predict(X_test)
+
+    oof_test[:] = oof_test_skf.mean(axis=0)
+    return oof_train.reshape(-1,1), oof_test.reshape(-1,1)
