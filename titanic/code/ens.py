77796791716b78b898c65b8cb33957bf92614416
ducksfrogs <ma_yamaki@yahoo.com>
2021-02-09 11:17:54 +0900

    d

--- titanic/code/ens.py
+++ titanic/code/ens.py
@@ -60,7 +60,32 @@ for dataset in full_data:
     dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
     dataset['Title'] = dataset['Title'].replace('Mne', 'Mrs')
 
+for  dataset in full_data:
+    dataset['Fare'] = dataset['Fare'].fillna(train['Fare'].median())
+    train['CategoricalFare'] = pd.qcut(train['Fare'], 4)
+
+for dataset in full_data:
+    age_avg = dataset['Age'].mean()
+    age_std = dataset['Age'].std()
+    age_null_count = dataset['Age'].isnull().sum()
+    
+
 for dataset in full_data:
     dataset['Sex'] = dataset['Sex'].map({"female": 0, "male": 1}).astype(int)
     title_mapping = {"Mr": 1, "Miss":2, "Mrs": 3, "Master":4, "Rare": 5}
     dataset['Title'] = dataset['Title'].map(title_mapping)
+    dataset['Title'] = dataset['Title'].fillna(0)
+
+    dataset['Embarked'] = dataset['Embarked'].map({"S": 0, "C":1, "Q":2}).astype(int)
+
+    dataset.loc[dataset['Fare'] <= 7.91, 'Fare'] = 0
+    dataset.loc[(dataset['Fare'] > 7.91) & (dataset['Fare'] <=14.45), 'Fare'] = 1
+    dataset.loc[(dataset['Fare'] > 14.45) & (dataset['Fare'] <= 31 ), 'Fare'] = 2
+    dataset.loc[(dataset['Fare'] > 31, 'Fare')] = 3
+    dataset['Fare'] = dataset['Fare'].astype(int)
+
+    dataset.loc[dataset['Age'<=16], 'Age'] = 0
+    dataset.loc[(dataset['Age'] >16) & (dataset['Age'] <= 32), 'Age'] = 1
+    dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <=48), 'Age'] = 2
+    dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
+    dataset.loc[(dataset['Age'] > 64) , 'Age'] = 4
