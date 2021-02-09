f69d7aa13632fc2994c51fac0ca0ce6c5de45685
ducksfrogs <ma_yamaki@yahoo.com>
2021-02-09 12:22:16 +0900

    ti

--- titanic/code/ens.py
+++ titanic/code/ens.py
@@ -43,6 +43,19 @@ for dataset in full_data:
 for dataset in full_data:
     dataset['Embarked'] = dataset['Embarked'].fillna('S')
 
+for dataset in full_data:
+    dataset['Fare'] = dataset['Fare'].fillna(train['Fare'].median())
+
+train['CategoricalFare']= pd.qcut(train['Fare'], 4)
+
+for dataset in full_data:
+    age_avg = dataset['Age'].mean()
+    age_std = dataset['Age'].std()
+    age_null_count = dataset['Age'].isnull().sum()
+    age_null_random_list = np.random.randint(age_avg - age_std, age_avg + age_std, size=age_null_count)
+    dataset['Age'][np.isnan(dataset['Age'])] = age_null_random_list
+    dataset['Age'] = dataset['Age'].astype(int)
+train['CategoricalAge'] = pd.cut(train['Age'], 5)
 
 def get_title(name):
     title_search = re.search(' ([A-Za-z]+)\.', name)
@@ -60,16 +73,6 @@ for dataset in full_data:
     dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
     dataset['Title'] = dataset['Title'].replace('Mne', 'Mrs')
 
-for  dataset in full_data:
-    dataset['Fare'] = dataset['Fare'].fillna(train['Fare'].median())
-    train['CategoricalFare'] = pd.qcut(train['Fare'], 4)
-
-for dataset in full_data:
-    age_avg = dataset['Age'].mean()
-    age_std = dataset['Age'].std()
-    age_null_count = dataset['Age'].isnull().sum()
-    
-
 for dataset in full_data:
     dataset['Sex'] = dataset['Sex'].map({"female": 0, "male": 1}).astype(int)
     title_mapping = {"Mr": 1, "Miss":2, "Mrs": 3, "Master":4, "Rare": 5}
@@ -84,8 +87,25 @@ for dataset in full_data:
     dataset.loc[(dataset['Fare'] > 31, 'Fare')] = 3
     dataset['Fare'] = dataset['Fare'].astype(int)
 
-    dataset.loc[dataset['Age'<=16], 'Age'] = 0
+    dataset.loc[dataset['Age']<=16, 'Age'] = 0
     dataset.loc[(dataset['Age'] >16) & (dataset['Age'] <= 32), 'Age'] = 1
     dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <=48), 'Age'] = 2
     dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
     dataset.loc[(dataset['Age'] > 64) , 'Age'] = 4
+
+drop_elemets = ['PassengerId', 'Name', 'Ticket', 'Cabin', 'SibSp']
+train =train.drop(drop_elemets,axis=1)
+train = train.drop(['CategoricalAge', 'CategoricalAge'], axis=1)
+test = test.drop(drop_elemets,axis=1)
+
+colormap = plt.cm.RdBu
+
+plt.figure(figsize=(14,12))
+plt.title("Peason Correlation of Features ", y=1.05, seze=15)
+sns.heatmap(train.astype(float).corr(), linewidths=0.1, vmax=1.0, square=True,
+            cmap=colormap, linecolor='white', annot=True)
+
+g = sns.pairplot(train[[u'Survived', u'Pclass', u'Sex', u'Age', u'Parch',
+                    u'Fare', u'Embarked', u'FamilySize', u'Title']], hue='Survived',palette='seismic',
+                    size=1.2, diag_kind='kde', diag_kws=dict(shade=True), plot_kws=dict(s=10))
+g.set(xticklabels=[])
