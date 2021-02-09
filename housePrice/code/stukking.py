153655e6810125032c40725a3c4bf135c02a4de0
ma <ma_yamaki@yahoo.com>
2021-02-09 16:45:18 +0900

    d

--- housePrice/code/stukking.py
+++ housePrice/code/stukking.py
@@ -40,3 +40,46 @@ plt.ylabel("SalePrice", fontsize=13)
 plt.xlabel("GrLivArea", fontsize=14)
 
 train = train.drop(train[(train['GrLivArea']>4000) & (train['SalePrice']<300000)].index)
+
+
+sns.distplot(train['SalePrice'], fit=norm)
+
+(mu, sigma) = norm.fit(train['SalePrice'])
+print('\n mu = {:.2f} and sigma = {:.2f}\n'.format(mu, sigma))
+
+plt.legend(['Normal dist. ($\mu=$ {:.2f} and $\sigma=$ {:.2f})'.format(mu, sigma)],
+            loc='best')
+
+plt.ylabel('Frequency')
+plt.title("SalePrice distribution")
+
+fig = plt.figure()
+res = stats.probplot(train['SalePrice'], plot=plt)
+
+
+train['SalePrice'] = np.log1p(train['SalePrice'])
+sns.distplot(train['SalePrice'], fit=norm)
+(mu, sigma) = norm.fit(train['SalePrice'])
+print('\n mu = {:.2f} and {:.2f}\n'.format(mu, sigma))
+plt.legend(['Normal dist. ($\mu$ {:.2f} and $\sigma=$ {:.2f})'.format(mu, sigma)],
+            loc='best')
+plt.ylabel('Frequency')
+plt.title("SalePrice distribution")
+
+fig = plt.figure()
+res = stats.probplot(train['SalePrice'], plot=plt)
+
+
+ntrain = train.shape[0]
+ntest = test.shape[0]
+
+y_train = train.SalePrice.values
+all_data = pd.concat((train, test)).reset_index(drop=True)
+
+all_data.drop(['SalePrice'], axis=1, inplace=True)
+print("all_data size is : {}".format(all_data.shape))
+
+all_data_na = (all_data.isnull().sum() / len(all_data)) *100
+all_data_na = all_data_na.drop(all_data_na[all_data_na==0].index).sort_values(ascending=False)[:30]
+
+missing_data = pd.DataFrame({ 'Missing Ratio': all_data_na})
