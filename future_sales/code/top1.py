b3b754cfd77edae961d4fe044d2513c7fa4fc5fb
ma <ma_yamaki@yahoo.com>
2021-02-27 06:04:09 +0900

    dd

--- future_sales/code/top1.py
+++ future_sales/code/top1.py
@@ -29,4 +29,34 @@ shops = pd.read_csv("../input/shops.csv")
 test = pd.read_csv("../input/test.csv")
 
 
-sales.date = sales.date.apply(lambda x: datetime.datetime.strptime(x, '%d.%m.%Y')) 
+sales.date = sales.date.apply(lambda x: datetime.datetime.strptime(x, '%d.%m.%Y'))
+
+
+monthly_sales = sales.groupby(['date_block_num', 'shop_id', 'item_id'])[ 'date','item_price',
+                                'item_cnt_day'].agg('date':['min', 'max'], 'item_price':'mean','item_cnt_day':'sum'))
+
+
+x = item.groupby(['item_category_id']).count()
+x = x.sort_values(by='item_id', ascending=False)
+x = x.iloc[0:10].reset_index()
+
+
+plt.figure(figsize=(8,4))
+ax = sns.barplot(x.item_category_id, x.item_id, alpha=0.8)
+plt.title("Items per Category")
+plt.ylabel("# of items", fontsize=12)
+plt.xlabel("Category", fontsize=12)
+plt.show()
+
+ts = sales.groupby(['date_block_num'])['item_cnt_day'].sum()
+ts.astype('float')
+plt.figure(figsiz=(16,8))
+plt.title("Total Sales of the company")
+plt.xlabel("Time")
+plt.ylabel("Sales")
+plt.plot(ts)
+
+plt.figure(figsize=(16,6))
+plt.plot(ts.rolling(window=12, center=False).mean(), label='Rolling Mean')
+plt.plot(ts.rolling(window=12, center=False).std(), label='Rolling sd')
+plt.legend()
